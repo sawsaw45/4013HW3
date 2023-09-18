f36b3fd1d4fc01d120b4c790accd39402bc731c6
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-18 14:03:04 -0500

    init commit

---
+++ view-footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+            <a class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" href="/">
+                <svg class="bi" height="24" width="30"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+            <span class="mb-3 mb-md-0 text-body-secondary">©Sawyer Hays <small><del>hey this counts as a row right??</del></small></span>
+        </div>
+    </footer>
+</div>
