1475216a0d2a80507e2d6c39c343faa5c5be0ecb
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-29 21:57:58 -0500

    brujhjj

--- model-workouts.php
+++ model-workouts.php
@@ -2,7 +2,7 @@
 function selectWorkouts() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT u.username, workoutname, date, Duration ,CaloriesBurned FROM Workouts w join users u on w.UserID = u.UserID");
+        $stmt = $conn->prepare("SELECT u.username, workoutname, date, Duration ,CaloriesBurned FROM Workouts w join Users u on w.UserID = u.UserID; ");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
