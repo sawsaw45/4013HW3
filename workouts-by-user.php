47863801987a3f87a3ea54adcb71c36eebd4b3a3
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-29 22:39:02 -0500

    im silly

--- workouts-by-user.php
+++ workouts-by-user.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-workouts-by-user.php");
 $pageTitle = "Workouts by User";
 include "view-header.php";
-$workouts = selectWorkoutsByUser($wid);
+$workouts = selectWorkoutsByUser($GET_['id']);
 include "view-workouts-by-user.php";
 include "view-footer.php";
 ?>
