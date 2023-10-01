caf1560fbabb7650fb3d2c826c3d9717f2ac9dfa
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-30 22:56:01 -0500

    hmmsss

--- view-details-by-workout.php
+++ view-details-by-workout.php
@@ -18,7 +18,7 @@
                     <td><?php echo $workout['sets']; ?></td>
                     <td><?php
                             $rep = $workout['repetitions'];
-                            if (isset($rep) or empty($rep))
+                            if (isset($rep) or $rep == '')
                         {
                             echo $rep;
                         }
