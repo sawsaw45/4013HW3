baf5f57be449b75909ed152479ef899b7c43c3e4
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-30 22:34:51 -0500

    does this work

--- details-by-workout.php
+++ details-by-workout.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-details-by-workout.php");
 $pageTitle = "Details by Workout";
 include "view-header.php";
-$workout = selectDetailsByWorkout($_GET['id']);
+$workouts = selectDetailsByWorkout($_GET['id']);
 include "view-details-by-workout.php";
 include "view-footer.php";
 ?><?php
