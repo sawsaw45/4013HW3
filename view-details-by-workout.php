c29d8bb853f577ed89069a1b552ff57b485dde24
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-30 22:44:38 -0500

    45511561

--- view-details-by-workout.php
+++ view-details-by-workout.php
@@ -1,7 +1,7 @@
 <div class="container">
     <?php while($workout = $workouts->fetch_assoc()) { ?>
     <h1><?php echo $workout['username']?> <?php echo $workout['workoutname']?> </h1>
-<?php } ?>
+
     <div class="table-responsive">
         <table class="table">
             <thead class="border border-danger rounded">
@@ -13,9 +13,7 @@
             </tr>
             </thead>
             <tbody>
-            <?php
-            while($workout = $workouts->fetch_assoc()) {
-                ?>
+
                 <tr class="border border-dark">
                     <td><?php echo $workout['sets']; ?></td>
                     <td><?php echo $workout['reps']; ?></td>
