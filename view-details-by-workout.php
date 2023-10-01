cb33be00254dc5ebbe58f79b1368aab13c63fda4
sawsaw45 <sawyer.2002@hotmail.com>
2023-09-30 22:32:26 -0500

    php is hard bc what is even happening

--- view-details-by-workout.php
+++ view-details-by-workout.php
@@ -1,6 +1,7 @@
 <div class="container">
+    <?php while($workout = $workouts->fetch_assoc()) ?>
     <h1><?php echo $workout['username']?> <?php echo $workout['workoutname']?> </h1>
-
+<?php } ?>
     <div class="table-responsive">
         <table class="table">
             <thead class="border border-danger rounded">
@@ -12,14 +13,14 @@
             </tr>
             </thead>
             <tbody>
-
+<?php while($workout = $workouts->fetch_assoc()) ?>
                 <tr class="border border-dark">
                     <td><?php echo $workout['sets']; ?></td>
                     <td><?php echo $workout['repetitions']; ?></td>
                     <td><?php echo $workout['weight']; ?></td>
                     <td><?php echo $workout['duration']; ?></td>
                 </tr>
-
+<?php } ?>
             </tbody>
         </table>
     </div>
