59f71ffa0f527f2ae166439cd498f491c6dbb717
Sawyer Hays <sawyer.2002@hotmail.com>
2023-10-03 14:57:29 -0500

    Update model-details-by-workout.php

--- model-details-by-workout.php
+++ model-details-by-workout.php
@@ -2,7 +2,7 @@
 function selectDetailsByWorkout($wid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("Select workoutname, username, repetitions, sets, wd.weight, duration FROM Users u JOIN Workouts w ON u.UserID = w.UserID JOIN WorkoutDetails wd ON w.WorkoutID = wd.WorkoutID WHERE workoutdetailid = ?; ");
+        $stmt = $conn->prepare("Select workoutname, username, repetitions, sets, wd.weight, duration FROM Users u JOIN Workouts w ON u.UserID = w.UserID JOIN WorkoutDetails wd ON w.WorkoutID = wd.WorkoutID WHERE wd.workoutid = ?; ");
 
         $stmt->bind_param("i", $wid);
         $stmt->execute();
