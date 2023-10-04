8819f38670ffcbe742392afc09b48bbbed278dee
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-04 14:22:14 -0500

    test

--- view-workouts.php
+++ view-workouts.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h1>Workouts</h1>
-    <div class="card-group" style="display:inline-flex;" >
+    <div class="card-columns" style="display:inline-flex;" >
         <?php while($workout = $workouts->fetch_assoc()) { ?>
         <div class="card">
                 <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
