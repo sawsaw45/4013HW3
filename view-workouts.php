52835250b2d0c0c2ae040944e84a68c1d19b4945
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-04 14:55:41 -0500

    oof 5

--- view-workouts.php
+++ view-workouts.php
@@ -1,31 +1,36 @@
 <div class="container">
     <h1>Workouts</h1>
-    <div class="card-group" >
-        <?php while($workout = $workouts->fetch_assoc()) { ?>
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $workout['username']?> <?php echo $workout['workoutname']?></h5>
-                <h6 class="card-subtitle mb-2 text-muted"><?php echo $workout['date'] ?></h6>
-                <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
-                while($workoutDeet = $workoutDeets->fetch_assoc()) { ?>
-                    <p class="card-text">Reps: <?php
-                        if ($workoutDeet['repetitions'] == 0 OR $workoutDeet['repetitions'] == NULL) {
-                            echo "N/A";
-                        } else {
-                            echo $workoutDeet['repetitions'];
-                        }
-                        ?>, Sets: <?php echo $workoutDeet['sets']?>, Weight: <?php
-                        if ($workoutDeet['weight'] == 0 OR $workoutDeet['weight'] == NULL) {
-                            echo "N/A";
-                        } else {
-                            echo number_format($workoutDeet['weight']);
-                        }
-                        ?></p>
+    <div class="row row-cols-1 row-cols-md-2 g-4" >
+        <div class="col">
+            <?php while($workout = $workouts->fetch_assoc()) { ?>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $workout['username']?> <?php echo $workout['workoutname']?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $workout['date'] ?></h6>
+                    <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
+                    while($workoutDeet = $workoutDeets->fetch_assoc()) { ?>
+                        <p class="card-text">Reps: <?php
+                            if ($workoutDeet['repetitions'] == 0 OR $workoutDeet['repetitions'] == NULL) {
+                                echo "N/A";
+                            } else {
+                                echo $workoutDeet['repetitions'];
+                            }
+                            ?>, Sets: <?php echo $workoutDeet['sets']?>, Weight: <?php
+                            if ($workoutDeet['weight'] == 0 OR $workoutDeet['weight'] == NULL) {
+                                echo "N/A";
+                            } else {
+                                echo number_format($workoutDeet['weight']);
+                            }
+                            ?></p>
 
-                <?php } ?>
+                    <?php } ?>
+                </div>
             </div>
-        </div>
+
+
             <?php } ?>
 
     </div>
-</div>
+
+    </div>
+    </div>
