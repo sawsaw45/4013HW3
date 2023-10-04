3633fd290c9d13c31dea130c31f31db29a8c4f25
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-04 13:41:01 -0500

    test2.5

--- view-workouts.php
+++ view-workouts.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h1>Workouts</h1>
-    <!--<div class="card-group" >-->
+    <div class="card-group" >
         <?php while($workout = $workouts->fetch_assoc()) { ?>
         <div class="card">
                 <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
@@ -27,5 +27,4 @@
         </div>
 <?php }?>
     </div>
-<!--</div>-
-->
+</div>
