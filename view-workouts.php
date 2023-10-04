66a57526dedba9198b78c46f68e76b5309a4aa8f
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-04 14:25:07 -0500

    eh

--- view-workouts.php
+++ view-workouts.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h1>Workouts</h1>
-    <div class="card-columns" >
+    <div class="container-fluid" >
         <?php while($workout = $workouts->fetch_assoc()) { ?>
         <div class="card">
                 <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
