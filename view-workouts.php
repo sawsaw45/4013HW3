3be37e6ac7146371b6dc2efd1cde466986ddb42d
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-04 13:40:20 -0500

    test2

--- view-workouts.php
+++ view-workouts.php
@@ -3,11 +3,11 @@
     <!--<div class="card-group" >-->
         <?php while($workout = $workouts->fetch_assoc()) { ?>
         <div class="card">
+                <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
+                 while($workoutDeet = $workoutDeets->fetch_assoc()) { ?>
             <div class="card-body">
                  <h5 class="card-title"><?php echo $workout['username']?> <?php echo $workout['workoutname']?></h5>
                 <h6 class="card-subtitle mb-2 text-muted"><?php echo $workout['date'] ?></h6>
-                <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
-                 while($workoutDeet = $workoutDeets->fetch_assoc()) { ?>
                 <p class="card-text">Reps: <?php
                     if ($workoutDeet['repetitions'] == 0 OR $workoutDeet['repetitions'] == NULL) {
                         echo "N/A";
