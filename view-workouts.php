84591870403612ea40650fd5401348dd141cfd2f
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-03 23:24:19 -0500

    uhhhhhh pht 6

--- view-workouts.php
+++ view-workouts.php
@@ -3,7 +3,7 @@
     <div class="card-group">
         <?php while($workout = $workouts->fetch_assoc()) { ?>
         <div class="card" style="width: 18rem;">
-            <div class="card-body">
+            <div class="card-body" style="max-width: 284.6px;">
                 <h5 class="card-title"><?php echo $workout['username']?> <?php echo $workout['workoutname']?></h5>
                 <h6 class="card-subtitle mb-2 text-muted"><?php echo $workout['date'] ?></h6>
                 <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
