1496361059893b016febe1c294900466a547d419
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-04 14:41:14 -0500

    oof

--- view-workouts.php
+++ view-workouts.php
@@ -1,106 +1,31 @@
 <div class="container">
     <h1>Workouts</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div class="card-columns" >
+    <div class="card-group" >
         <?php while($workout = $workouts->fetch_assoc()) { ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    <div class="container-fluid" >
-        <?php while($workout = $workouts->fetch_assoc()) { ?>
->>>>>>> parent of 5f51174 (mega test)
-=======
-    <div class="container-fluid" >
-        <?php while($workout = $workouts->fetch_assoc()) { ?>
->>>>>>> parent of 5f51174 (mega test)
-=======
-    <div class="container-fluid" >
-        <?php while($workout = $workouts->fetch_assoc()) { ?>
->>>>>>> parent of 5f51174 (mega test)
         <div class="card">
-                <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
-                 while($workoutDeet = $workoutDeets->fetch_assoc()) { ?>
             <div class="card-body">
-                 <h5 class="card-title"><?php echo $workout['username']?> <?php echo $workout['workoutname']?></h5>
+                <h5 class="card-title"><?php echo $workout['username']?> <?php echo $workout['workoutname']?></h5>
                 <h6 class="card-subtitle mb-2 text-muted"><?php echo $workout['date'] ?></h6>
-                <p class="card-text">Reps: <?php
-                    if ($workoutDeet['repetitions'] == 0 OR $workoutDeet['repetitions'] == NULL) {
-                        echo "N/A";
-                    } else {
-                        echo $workoutDeet['repetitions'];
-                    }
-                    ?>, Sets: <?php echo $workoutDeet['sets']?>, Weight: <?php
-                    if ($workoutDeet['weight'] == 0 OR $workoutDeet['weight'] == NULL) {
-                        echo "N/A";
-                    } else {
-                        echo number_format($workoutDeet['weight']);
-                    }
-                    ?></p>
-<?php } ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of c449aaa (mega test 1)
-=======
->>>>>>> parent of c449aaa (mega test 1)
-=======
->>>>>>> parent of c449aaa (mega test 1)
-            <div class="col-sm-4">
-                <div class="card">
-                    <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
-                    while($workoutDeet = $workoutDeets->fetch_assoc()) { ?>
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $workout['username']?> <?php echo $workout['workoutname']?></h5>
-                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $workout['date'] ?></h6>
-                        <p class="card-text">Reps: <?php
-                            if ($workoutDeet['repetitions'] == 0 OR $workoutDeet['repetitions'] == NULL) {
-                                echo "N/A";
-                            } else {
-                                echo $workoutDeet['repetitions'];
-                            }
-                            ?>, Sets: <?php echo $workoutDeet['sets']?>, Weight: <?php
-                            if ($workoutDeet['weight'] == 0 OR $workoutDeet['weight'] == NULL) {
-                                echo "N/A";
-                            } else {
-                                echo number_format($workoutDeet['weight']);
-                            }
-                            ?></p>
-                        <?php } ?>
->>>>>>> parent of c449aaa (mega test 1)
+                <?php $workoutDeets = selectWorkoutDeets($workout['workoutid']);
+                while($workoutDeet = $workoutDeets->fetch_assoc()) { ?>
+                    <p class="card-text">Reps: <?php
+                        if ($workoutDeet['repetitions'] == 0 OR $workoutDeet['repetitions'] == NULL) {
+                            echo "N/A";
+                        } else {
+                            echo $workoutDeet['repetitions'];
+                        }
+                        ?>, Sets: <?php echo $workoutDeet['sets']?>, Weight: <?php
+                        if ($workoutDeet['weight'] == 0 OR $workoutDeet['weight'] == NULL) {
+                            echo "N/A";
+                        } else {
+                            echo number_format($workoutDeet['weight']);
+                        }
+                        ?></p>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    </div>
-                </div>
+                <?php } ?>
             </div>
-
-
-=======
-<?php } ?>
-            </div>
->>>>>>> parent of a405b39 (noooooo)
-=======
-            </div>
->>>>>>> parent of 5f51174 (mega test)
-=======
-
-            </div>
->>>>>>> parent of 5f51174 (mega test)
-=======
-
-            </div>
->>>>>>> parent of 5f51174 (mega test)
-=======
-
-            </div>
->>>>>>> parent of 5f51174 (mega test)
+            <?php } ?>
         </div>
-<?php }?>
+        <?php }?>
     </div>
 </div>
