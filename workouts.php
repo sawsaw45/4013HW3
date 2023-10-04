8cfb732ef6bd13bf2a0b25022d6096c83e789ebf
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-03 23:16:21 -0500

    uhhhhhh

--- workouts.php
+++ workouts.php
@@ -4,7 +4,6 @@ require_once("model-workouts.php");
 $pageTitle = "Workouts";
 include "view-header.php";
 $workouts = selectWorkouts();
-$workoutDeets = selectWorkoutDeets($wid);
 include "view-workouts.php";
 include "view-footer.php";
 ?>
