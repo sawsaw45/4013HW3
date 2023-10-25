d39e151d36ea9d092e2ec371873a4b7d316893f4
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-25 10:03:30 -0500

    deletion not breaky

--- view-workouts-by-user.php
+++ view-workouts-by-user.php
@@ -39,6 +39,7 @@
                         <form method="post" action="">
                             <input type="hidden" name="id" value="<?php echo $workout['workoutid']; ?>">
                             <input type="hidden" name="actionType" value="Delete">
+                            <input type="hidden" name="cid" value="<?php echo $_POST['cid']; ?>" />
                             <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-x" viewBox="0 0 16 16">
                                     <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm.256 7a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
