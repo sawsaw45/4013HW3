4e44e30e3b5f17b082475d96af9ecb198782bc38
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-25 11:48:32 -0500

    hmmm

--- model-details-by-workout.php
+++ model-details-by-workout.php
@@ -18,8 +18,7 @@ function insertDetailsByWorkout($wid, $eid, $sets, $reps, $weight) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO `WorkoutDetails` (`WorkoutID`, `ExerciseID`, `Sets`, `Repetitions`, `Weight`) VALUES (NULL, NULL, NULL, NULL, NULL) ");
-
-        $stmt->bind_param("isssd", $wid , $eid, $sets, $reps, $weight);
+        $stmt->bind_param("iiiid", $wid , $eid, $sets, $reps, $weight);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
