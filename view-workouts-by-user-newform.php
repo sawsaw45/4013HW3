4c7e55c7f2522c499e57e7ce6d8c913a89ebe430
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-24 22:14:29 -0500

    SCARED

--- view-workouts-by-user-newform.php
+++ view-workouts-by-user-newform.php
@@ -38,8 +38,11 @@
                         <label for="CaloriesBurned" class="form-label">Calories Burned</label>
                         <input type="text" class="form-control" id="CaloriesBurned" name="CaloriesBurned">
                     </div>
-                    <input type="hidden" name="cid" value="<?php echo $user['UserID']; ?>">
+
                     <input type="hidden" name="actionType" value="add">
+                    <form method="post" action="workouts-by-user.php">
+                        <input type="hidden" name="cid" value="<?php echo $user['UserID']; ?>">
+
                     <button type="submit" class="btn btn-primary">Save</button>
                 </form>
             </div>
