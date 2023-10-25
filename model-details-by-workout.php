1f79c8ac4ec4a8f1f05939475f9d8fbbc2107c27
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-25 14:24:56 -0500

    not my fault this time

--- model-details-by-workout.php
+++ model-details-by-workout.php
@@ -3,7 +3,6 @@ function selectDetailsByWorkout($wid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("Select workoutdetailid, workoutid, workoutname, username, repetitions, sets, wd.weight, duration FROM Users u JOIN Workouts w ON u.UserID = w.UserID JOIN WorkoutDetails wd ON w.WorkoutID = wd.WorkoutID WHERE wd.workoutid = ?; ");
-
         $stmt->bind_param("i", $wid);
         $stmt->execute();
         $result = $stmt->get_result();
