ae91b673049c669c16b35fb19a5ae76573bd264d
sawsaw45 <sawyer.2002@hotmail.com>
2023-10-25 22:33:45 -0500

    intellij was tooo fast for me

--- exercises.php
+++ exercises.php
@@ -19,7 +19,7 @@ if (isset($_POST['actionType'])) {
                 echo "<div class='alert alert-danger' role='alert'>Error deleting exercise</div>";
             }
             break;
-        case "Update":
+        case "Edit":
             if ($success = updateExercise($_POST['exercisename'], $_POST['description'], $_POST['musclegroup'], $_POST['equipmentrequired'], $_POST['id'])) {
                 echo "<div class='alert alert-success' role='alert'>Exercise updated successfully</div>";
             } else {
